1c26d54d7689753867458acd1cb9f205b350d001
mhhuijsmans <>
2020-02-27 01:19:22 +0100

    clamp.js

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -1,10 +1,3 @@
-/*
-  Wat doe je ook alweer in Javascript voor een micro-interactie?
-  1. Gebruik de querySelector om een element in je html te selecteren
-  2. Koppel een evenListener aan het element om een mouse-event te detecteren
-  3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
-*/
-
 var el = document.getElementsByClassName("playlist")[0];
 var sortable = new Sortable(el, {
 	ghostClass: 'ghost',
@@ -13,3 +6,9 @@ var sortable = new Sortable(el, {
     delay: 200, // time in milliseconds to define when the sorting should start
 	delayOnTouchOnly: true
 });
+
+var clamp = document.querySelectorAll(".song > *");
+
+for (i = 0; i < clamp.length; i++) {
+    $clamp(clamp[i], {clamp: 2});
+}
