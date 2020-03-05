e2f34b9a9ee12e0946a6dd4ffdd63fcfb0028afd
mhhuijsmans <mirthe_93_mirthe@hotmail.com>
2020-03-05 17:53:34 +0100

    custom navigation js + positioning

--- opdracht2/js/script.js
+++ opdracht2/js/script.js
@@ -1,5 +1,54 @@
+var utils = window.fizzyUIUtils;
+
 var carousel = document.querySelector('.carousel');
 var flickity = new Flickity( carousel, {
-  cellAlign: 'left',
-  wrapAround: true
+	cellAlign: 'left',
+	wrapAround: true,
+	prevNextButtons: false,
+	pageDots: false
+});
+
+// dynamically making dots
+var photos = document.querySelectorAll('.item');
+
+for (i = 0; i < photos.length; i++) {
+	var li = document.createElement('li');
+	li.className = 'dot dot' + i;
+
+	document.querySelector('.dots').appendChild(li);
+}
+
+document.querySelector('.dot').classList.add('is-selected');
+
+// elements
+var cellsButtonGroup = document.querySelector('.dots');
+var cellsButtons = utils.makeArray(cellsButtonGroup.children);
+
+// update buttons on select
+flickity.on( 'select', function() {
+	var previousSelectedButton = cellsButtonGroup.querySelector('.is-selected');
+	var selectedButton = cellsButtonGroup.children[ flickity.selectedIndex ];
+	previousSelectedButton.classList.remove('is-selected');
+	selectedButton.classList.add('is-selected');
+});
+
+// cell select
+cellsButtonGroup.addEventListener('click', function(event) {
+	if (!matchesSelector( event.target, '.dot')) {
+		return;
+	}
+	var index = cellsButtons.indexOf(event.target);
+	flickity.select(index);
+});
+
+// previous
+var previousButton = document.querySelector('.button-previous');
+previousButton.addEventListener('click', function() {
+	flickity.previous();
+});
+
+// next
+var nextButton = document.querySelector('.button-next');
+nextButton.addEventListener('click', function() {
+	flickity.next();
 });
